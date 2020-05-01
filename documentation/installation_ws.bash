2e2a6ece0d991044cc8756e07d67834d733277bb
Rattiputz <rezzo.steam@gmail.com>
2020-05-01 14:40:30 +0200

    Documentation

--- documentation/installation_ws.bash
+++ documentation/installation_ws.bash
@@ -1,7 +1,6 @@
-git clone https://github.com/Rattiputz/plateforme-robotique-mobile.git
-mkdir -p prm_ws/src
-cd prm_ws
-catkin_make
+git clone https://github.com/Rattiputz/plateforme-robotique-mobile.git ~/plateforme-robotique-mobile
+mkdir -p ~/prm_ws/src
 cp -r ~/plateforme-robotique-mobile/prm_ws/src ~/prm_ws/
-catkin_make
+cd ~/prm_ws
+catkin_make -DCMAKE_BUILD_TYPE=Release
 echo "source ~/prm_ws/devel/setup.bash" >> ~/.bashrc
